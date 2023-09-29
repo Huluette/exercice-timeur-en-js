0d36d774b4d3bae94f24b841c80630b60eae398e
Huluette <leana.beuvant@gmail.com>
2023-09-29 15:43:58 +0200

    test exo

--- main.js
+++ main.js
@@ -29,15 +29,49 @@
 
 // TEST 2 
 
-function decompte(n) {
-    console.log(n)
-    const t = setInterval(() => {
-    n--
-    console.log(n)
-        if (n === 0) {
-        clearInterval(t)
-    }
-}, 1000)
-}
+// function decompte(n) {
+//     console.log(n)
+//     const t = setInterval(() => {
+//     n--
+//     console.log(n)
+//         if (n === 0) {
+//         clearInterval(t)
+//     }
+// }, 1000)
+// }
 
-decompte(5)
+// decompte(3)
+
+// TEST 3 
+
+// function decompte(n) {
+//     // console.log(n)
+//     // if (n === 0) {
+//     //     return
+//     // }
+    
+//     setTimeout(() => {
+
+//         decompte(n - 1)
+//     }, 1000)
+// }
+
+// decompte(3)
+
+// TEST 4 
+
+const now = new Date().getTime();
+const countdownDate = new Date('September 29, 2023').getTime();
+
+// console.log(now, countdownDate);
+
+const distanceBase = countdownDate - now;
+
+const days = Math.floor(distanceBase / (1000 * 60 * 60 * 24));
+// console.log(days);
+const hours = Math.floor((distanceBase % (1000 * 60 * 60 * 24)) /
+(1000 * 60 * 60))
+const minutes = Math.floor((distanceBase % (1000 * 60 * 60)) / (1000 * 60))
+const seconds = Math.floor((distanceBase % (1000 * 60)) / 1000);
+
+// console.log(days, hours, minutes, seconds);

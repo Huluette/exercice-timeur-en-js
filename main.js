7cf8a8a9010ce53c81cf7f2f937781cf6eedef94
Huluette <leana.beuvant@gmail.com>
2023-09-29 14:12:07 +0200

    troisieme test

--- main.js
+++ main.js
@@ -16,3 +16,28 @@
 
 // TEST 
 
+// let i = 0
+
+// const t = setInterval(() => {
+//     i++
+//     console.log('bonjour')
+
+//     if (i >= 5) {
+//         clearInterval(t)
+//     }
+// }, 1000);
+
+// TEST 2 
+
+function decompte(n) {
+    console.log(n)
+    const t = setInterval(() => {
+    n--
+    console.log(n)
+        if (n === 0) {
+        clearInterval(t)
+    }
+}, 1000)
+}
+
+decompte(5)
